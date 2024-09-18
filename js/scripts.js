ca08d62d6668bc7e04deeca1fec805c9aead0082
NiStein <nicolassales204@gmail.com>
2024-09-18 16:22:51 -0300

    Commit 6

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ const courses = document.querySelectorAll(".course");
 
 // Adiciona um evento de clique para cada curso
 courses.forEach(course => {
-    course.addEventListener("click", function() {
+    course.addEventListener("click", function () {
         // Remove a classe "selected" de todos os cursos
         courses.forEach(c => c.classList.remove("selected"));
-        
+
         // Adiciona a classe "selected" apenas ao curso clicado
         this.classList.add("selected");
     });
@@ -15,7 +15,7 @@ courses.forEach(course => {
 function toggleText() {
     const moreText = document.querySelector('.more-text');
     const button = document.querySelector('.show-more-button');
-    
+
     if (moreText.classList.contains('hidden')) {
         moreText.classList.remove('hidden');
         button.textContent = 'Mostrar menos';
@@ -47,7 +47,7 @@ function openTab(evt, tabName) {
 
 function atualizarValoresIngressos() {
     const dataAtual = new Date();
-    
+
     const lote1Fim = new Date('2024-10-31');
     const lote2Inicio = new Date('2024-11-01');
     const lote2Fim = new Date('2024-12-31');
@@ -55,11 +55,11 @@ function atualizarValoresIngressos() {
     const lote3Fim = new Date('2025-02-28');
     const lote4Inicio = new Date('2025-03-01');
     const lote4Fim = new Date('2025-05-25')
-    
+
     const estudanteBox = document.querySelector('.card.studant p');
     const demaisBox = document.querySelector('.card.entire p');
 
-    
+
     if (dataAtual <= lote1Fim) {
         estudanteBox.textContent = 'R$ 130,00';
         demaisBox.textContent = 'R$ 250,00';
@@ -80,7 +80,7 @@ function atualizarValoresIngressos() {
 
 function atualizarValoresIngressosOficinas() {
     const dataAtual = new Date();
-    
+
     const lote1Fim = new Date('2024-10-31');
     const lote2Inicio = new Date('2024-11-01');
     const lote2Fim = new Date('2024-12-31');
@@ -88,11 +88,11 @@ function atualizarValoresIngressosOficinas() {
     const lote3Fim = new Date('2025-02-28');
     const lote4Inicio = new Date('2025-03-01');
     const lote4Fim = new Date('2025-05-25')
-    
+
     const estudanteBox = document.querySelector('.card.studantofice p');
     const demaisBox = document.querySelector('.card.entireofice p');
 
-    
+
     if (dataAtual <= lote1Fim) {
         estudanteBox.textContent = 'R$ 30,00';
         demaisBox.textContent = 'R$ 50,00';
@@ -113,7 +113,7 @@ function atualizarValoresIngressosOficinas() {
 
 function atualizarValoresTrabalhos() {
     const dataAtual = new Date();
-    
+
     const lote1Fim = new Date('2024-10-31');
     const lote2Inicio = new Date('2024-11-01');
     const lote2Fim = new Date('2024-12-31');
@@ -121,24 +121,24 @@ function atualizarValoresTrabalhos() {
     const lote3Fim = new Date('2025-02-28');
     const lote4Inicio = new Date('2025-03-01');
     const lote4Fim = new Date('2025-05-25')
-    
+
     const unicaBox = document.querySelector('.card.unica p');
 
-    
+
     if (dataAtual <= lote1Fim) {
-        unicaBox.textContent = 'R$ 130,00';
+        unicaBox.textContent = 'R$ 125,00';
     } else if (dataAtual >= lote2Inicio && dataAtual <= lote2Fim) {
-        unicaBox.textContent = 'R$ 200,00';
+        unicaBox.textContent = 'R$ 150,00';
     } else if (dataAtual >= lote3Inicio && dataAtual <= lote3Fim) {
-        unicaBox.textContent = 'R$ 250,00';
+        unicaBox.textContent = 'R$ 175,00';
     } else if (dataAtual >= lote4Inicio && dataAtual <= lote4Fim) {
-        unicaBox.textContent = 'R$ 300,00'
+        unicaBox.textContent = 'R$ 200,00'
     } else if (dataAtual > lote4Fim) {
         unicaBox.textContent = 'Inscrições Encerradas'
     }
 };
 
-function countdown(){
+function countdown() {
     var SEC = 1000;
     var MIN = 60 * SEC;
     var HR = 60 * MIN;
@@ -153,35 +153,35 @@ function countdown(){
     initCountdown()
 
     function initCountdown() {
-    var countdownDate = "Tue May 27 2025 08:00:00 GMT-0300 (Hora padrão de Brasília)"
-    
-    interval = setInterval(() => updateCounter(new Date(countdownDate)), 1000);
+        var countdownDate = "Tue May 27 2025 08:00:00 GMT-0300 (Hora padrão de Brasília)"
+
+        interval = setInterval(() => updateCounter(new Date(countdownDate)), 1000);
     }
 
     function updateCounter(dateTarget) {
-    var countdown = calculateTimeDifference(dateTarget);
+        var countdown = calculateTimeDifference(dateTarget);
 
-    dayElement.innerHTML = countdown.days;
-    hourElement.innerHTML = countdown.hours;
-    minuteElement.innerHTML = countdown.minutes;
-    secondElement.innerHTML = countdown.seconds;
+        dayElement.innerHTML = countdown.days;
+        hourElement.innerHTML = countdown.hours;
+        minuteElement.innerHTML = countdown.minutes;
+        secondElement.innerHTML = countdown.seconds;
     };
 
     function calculateTimeDifference(dateTarget) {
-    var currentTime = new Date().getTime();
-    var difference = dateTarget.getTime() - currentTime;
+        var currentTime = new Date().getTime();
+        var difference = dateTarget.getTime() - currentTime;
 
-    if (difference < 0) {
-        clearInterval(interval)
-        return { days: '00', hours: '00', minutes: '00', seconds: '00' };
-    }
+        if (difference < 0) {
+            clearInterval(interval)
+            return { days: '00', hours: '00', minutes: '00', seconds: '00' };
+        }
 
-    var days = `${Math.floor(difference / DAY)}`.padStart(2, '0');
-    var hours = `${Math.floor((difference % DAY) / HR)}`.padStart(2, '0');
-    var minutes = `${Math.floor((difference % HR) / MIN)}`.padStart(2, '0');
-    var seconds = `${Math.floor((difference % MIN) / SEC)}`.padStart(2, '0');
+        var days = `${Math.floor(difference / DAY)}`.padStart(2, '0');
+        var hours = `${Math.floor((difference % DAY) / HR)}`.padStart(2, '0');
+        var minutes = `${Math.floor((difference % HR) / MIN)}`.padStart(2, '0');
+        var seconds = `${Math.floor((difference % MIN) / SEC)}`.padStart(2, '0');
 
-    return { days: days, hours: hours, minutes: minutes, seconds: seconds };
+        return { days: days, hours: hours, minutes: minutes, seconds: seconds };
     };
 };
 
@@ -195,5 +195,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // Abre a primeira tab (EMENTA) por padrão e adiciona a classe "active" ao primeiro link
     document.getElementById("defaultOpen").click();
     atualizarValoresIngressos();
+    atualizarValoresIngressosOficinas();
+    atualizarValoresTrabalhos();
     countdown();
 });
